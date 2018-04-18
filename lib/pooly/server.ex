ca97b4d5ef679796b9ad525f7006ecc56f960225
Iggy <igoririanto@rocketmail.com>
2018-04-18 17:51:16 -0500

    add prepopulate

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -37,11 +37,28 @@ defmodule Pooly.Server do
   def handle_info(:start_worker_supervisor, state = %{sup: sup, mfa: mfa, size: size}) do
     {:ok, worker_sup} = Supervisor.start_child(sup, supervisor_spec(mfa))
     workers = prepopulate(size, worker_sup)
-    {:noreply, %{state | worker_sup: worker_sup, workers: workers})
+    {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
   end
 
   defp supervisor_spec(mfa) do
-    opts = [restart: temporary]
+    opts = [restart: :temporary]
     supervisor(Pooly.WorkerSupervisor, [mfa], opts)
+  end
+
+  defp prepopulate(size, sup) do
+    prepopulate(size, sup, [])
+  end
+
+  defp prepopulate(size, _sup, workers) when size < 1 do
+    workers
+  end
+
+  defp prepopulate(size, sup, workers) do
+    prepopulate(size - 1, sup, [new_worker(sup) | workers])
+  end
+
+  defp new_worker(sup) do
+    {:ok, worker} = Supervisor.start_child(sup, [[]])
+    worker
   end
 end
