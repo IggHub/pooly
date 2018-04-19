aedbee8182b2a0ca8d2b9ea28f4fec98d93ef18c
Iggy <igoririanto@rocketmail.com>
2018-04-19 13:41:54 -0500

    fix syntax error, edit mix.exs and pooly.ex

--- lib/pooly/supervisor.ex
+++ lib/pooly/supervisor.ex
@@ -11,7 +11,7 @@ defmodule Pooly.Supervisor do
 
   def init(pool_config) do
     children = [
-      worker(Pooly.Server, [self, pool_config])
+      worker(Pooly.Server, [self(), pool_config])
     ]
     opts = [strategy: :one_for_all]
     supervise(children, opts)
