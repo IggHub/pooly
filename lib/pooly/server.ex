97f10f0eedafca909f13a0a46c71eb39b26a3154
Iggy <igoririanto@rocketmail.com>
2018-04-19 13:19:54 -0500

    add status to check available workers and checked out workers

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -19,6 +19,10 @@ defmodule Pooly.Server do
     GenServer.cast(__MODULE__, {:checkin, worker_pid})
   end
 
+  def status do
+    GenServer.call(__MODULE__, :status)
+  end
+
   # Callbacks
 
   def init([sup, pool_config]) when is_pid(sup)  do
@@ -58,6 +62,10 @@ defmodule Pooly.Server do
       [] ->
         {:reply, :noproc, state}
     end
+  end
+
+  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
+    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
   end
 
   def handle_cast({:checkin, worker}, %{workers: workers, monitors: monitors} = state) do
